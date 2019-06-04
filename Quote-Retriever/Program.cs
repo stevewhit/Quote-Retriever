b001e70771f84f91bc1acad172a3b647e7fa3289
stevewhit <swhit114@gmail.com>
2019-06-04 15:57:05 -0400

    Initial project creation.

---
+++ Quote-Retriever/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quote_Retriever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
